e57ea940100154e08822c62a469b1e1cbc816590
Janos Kocs <janos.kocs@outlook.com>
2022-07-21 13:45:16 +0100

    Basic error handling and loading when fetching

--- src/Form.js
+++ src/Form.js
@@ -4,21 +4,62 @@ import { useEffect, useState } from 'react'
 
 const Form = () => {
     const [activity, setActivity] = useState('')
+    const [error, setError] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [category, setCategory] = useState('')
+
+
+    const controller = new AbortController()
+    const signal = controller.signal
+    const url = 'http://www.boredapi.com/api/activity/'
 
     const fetchIdea = () => {
-        fetch('http://www.boredapi.com/api/activity/')
-            .then(res => res.json())
-            .then(data => setActivity(data))
-        console.log(activity)
+        fetch(url, {
+            method: 'get',
+            signal: signal
+        }).then(function (response) {
+            if (response.status === 200) {
+                setIsLoading(false)
+                return response.json()
+            } else {
+                setError(true)
+                controller.abort()
+                setIsLoading(false)
+                return false
+            }
+        }).then(data => setActivity(data))
     }
+
     useEffect(() => {
         fetchIdea()
     }, [])
 
+
+
     return (
         <div id="activity">
             <h2>Consider doing the following</h2>
-            <h3>{activity.activity}</h3>
+            {!isLoading ? <h3>{activity.activity}</h3> : <h3>Loading</h3>}
+            {error && <h3>Something went wrong, please try again later.</h3>}
+
+            <div id="customizeActivity">
+                <div id="price-selector">
+                    <label htmlFor="priceSelection">Select price for desired activity</label>
+                    <input type="range" id="priceSelection" min="0" max="1" step="0.05" />
+                </div>
+
+                <div id="categories">
+                    <button>Education</button>
+                    <button>Recreational</button>
+                    <button>Social</button>
+                    <button>DIY</button>
+                    <button>Charity</button>
+                    <button>Cooking</button>
+                    <button>Relaxation</button>
+                    <button>Music</button>
+                    <button>Busywork</button>
+                </div>
+            </div>
             <button onClick={fetchIdea}>Mmmm, give me another idea!</button>
         </div>
     )
